9b4b4d3da5cb7af3d5c10fa98d86039b8dc21795
Henrique Gonçalves de Oliveira <48062168+HenriqueGon@users.noreply.github.com>
2022-06-06 21:01:57 -0300

    2022-06-06

--- Balufy/Balufy/settings.py
+++ Balufy/Balufy/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'cadastros.apps.CadastrosConfig',
+    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Views Login
+
+LOGIN_URL = 'login' 
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
