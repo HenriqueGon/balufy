69f86092231a903e6086cdc0add024d649ae0116
Henrique Gonçalves de Oliveira <48062168+HenriqueGon@users.noreply.github.com>
2022-06-13 20:38:44 -0300

    Atividade 13/06

--- Balufy/cadastros/urls.py
+++ Balufy/cadastros/urls.py
@@ -1,5 +1,30 @@
 from django.urls import path
 
+from .views import IndexView, PlaylistCreate, AuthorCreate, SongCreate
+from .views import PlaylistUpdate, AuthorUpdate, SongUpdate
+from .views import PlaylistDelete, AuthorDelete, SongDelete
+
 urlpatterns = [
-    # path('', IndexView.as_view(), name="view")
+    path('', IndexView.as_view(),
+         name='index'),
+    path('cadastrar/playlist/', PlaylistCreate.as_view(),
+         name='cadastrar-playlist'),
+    path('cadastrar/autor/', AuthorCreate.as_view(), 
+          name='cadastrar-autor'),
+    path('cadastrar/musica/', SongCreate.as_view(),
+         name='cadastrar-musica'),
+
+    path('editar/playlist/<int:pk>/',
+         PlaylistUpdate.as_view(), name='editar-playlist'),
+    path('editar/autor/<int:pk>/',
+         AuthorUpdate.as_view(), name='editar-autor'),
+    path('editar/musica/<int:pk>/',
+         SongUpdate.as_view(), name='editar-musica'),
+
+    path('excluir/playlist/<int:pk>/',
+         PlaylistDelete.as_view(), name='excluir-playlist'),
+    path('excluir/autor/<int:pk>/',
+         AuthorDelete.as_view(), name='excluir-autor'),
+    path('excluir/musica/<int:pk>/',
+         SongDelete.as_view(), name='excluir-musica'),
 ]
--- Balufy/cadastros/views.py
+++ Balufy/cadastros/views.py
@@ -1,3 +1,87 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from dataclasses import field
+from pyexpat import model
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic import TemplateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from braces.views import GroupRequiredMixin
+
+from .models import Playlist, Author, Song
+
+# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'cadastros/index.html'
+
+class PlaylistCreate(LoginRequiredMixin, CreateView):
+    model = Playlist
+    fields = ['name', 'description']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('listar-playlist')
+    # login_url = reverse_lazy('login')
+
+
+class AuthorCreate(LoginRequiredMixin, CreateView):
+    model = Author
+    fields = ['name', 'description']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+
+class SongCreate(LoginRequiredMixin, CreateView):
+    model = Song
+    fields = ['name', 'duration', 'url']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+
+###################################
+
+
+class PlaylistUpdate(LoginRequiredMixin, UpdateView):
+    model = Playlist
+    fields = ['name', 'description']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('listar-playlist')
+
+
+class AuthorUpdate(LoginRequiredMixin, UpdateView):
+    model = Author
+    fields = ['name', 'description']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+
+class SongUpdate(LoginRequiredMixin, UpdateView):
+    model = Song
+    fields = ['name', 'description', 'url']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+####################################
+
+
+class PlaylistDelete(LoginRequiredMixin, DeleteView):
+    model = Playlist
+    template_name = 'cadastros/form-delete.html'
+    success_url = reverse_lazy('listar-playlist')
+
+
+class AuthorDelete(LoginRequiredMixin, DeleteView):
+    model = Author
+    template_name = 'cadastros/form-delete.html'
+    success_url = reverse_lazy('index')
+
+
+class SongDelete(LoginRequiredMixin, DeleteView):
+    model = Song
+    template_name = 'cadastros/form-delete.html'
+    success_url = reverse_lazy('index')
+
+
+#################################
